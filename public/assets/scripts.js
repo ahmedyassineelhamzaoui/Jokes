aeb4596b3922118aeaf5ade1148cbfabf89c6307
toufik1998 <toufikshima98@gmail.com>
2023-01-06 00:37:48 +0100

    add delete confirmation & some refactoring code

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -1,5 +1,18 @@
 let addJokeBtn = document.getElementById("task-update-btn");
+let updateJokeBtn = document.getElementById("task-save-btn");
 
-function hideOtherBtn() {
-    addJokeBtn.style.display = 'none';
+function hideOtherBtn(hide) {
+    if(hide == 1){
+        updateJokeBtn.style.display = 'block';
+        addJokeBtn.style.display = 'none';
+    }else{
+        addJokeBtn.style.display = 'block';
+        updateJokeBtn.style.display = 'none';
+    }
+}
+
+function deleteJoke(){
+    if(confirm('Do you want To delete this Product : ') == true){
+        document.querySelector("#buttonDelete").click();
+    }
 }
